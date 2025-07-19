2c3273f3c0cc706dbdde80362714f93e3752c818
dagegenihaoma <benjm240@gmail.com>
2025-07-19 15:25:51 +0800

    init bridge

---
+++ src/component/lifi-widget-wrapper.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { LiFiWidget, WidgetConfig, useWidgetEvents, useAvailableChains, ChainType, ChainId } from '@lifi/widget';
+import { useParams, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const SOLANA_CHAIN_ID = 1151111081099710;
+
+const widgetConfig: WidgetConfig = {
+  variant: "wide",
+  theme: {
+    container: {
+      border: '1px solid rgb(234, 234, 234)',
+      borderRadius: '16px',
+    },
+  },
+  chains: {
+    from: {
+      allow: [1, 8453, 10, 137, 324, 42161, 80094, 81457, 534352, 56],
+    },
+    to: {
+      allow: [SOLANA_CHAIN_ID],
+    },
+  },
+  fromChain: 1,
+  toChain: SOLANA_CHAIN_ID,
+  fromToken: "0x0000000000000000000000000000000000000000",
+  toToken: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+  toAddress: {
+    address: '0x0000000000000000000000000000000000000000',
+    chainType: ChainType.EVM,
+  },
+  tokens: {
+    to: {
+      allow: [{
+        chainId: SOLANA_CHAIN_ID,
+        address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+      }, {
+        chainId: SOLANA_CHAIN_ID,
+        address: 'zBTCug3er3tLyffELcvDNrKkCymbPWysGcWihESYfLg',
+      }],
+    },
+  },
+  hiddenUI: ['reverseTokensButton']
+};
+
+export default function LiFiWidgetWrapper() {
+  const params = useSearchParams();
+
+  const toAddress = params.get('toAddress')
+  const fromAmount = params.get('fromAmount')
+
+  const [config, setConfig] = useState(widgetConfig)
+
+  useEffect(() => {
+    if (!toAddress) return;
+    setConfig({
+      ...config,
+      toAddress: {
+        address: toAddress || '',
+        chainType: ChainType.EVM,
+      },
+      fromAmount: fromAmount || 0,
+    })
+  }, [toAddress, fromAmount])
+
+
+  
+  return (
+    <div>
+      <LiFiWidget integrator="dolla"  config={config} />
+    </div>
+  );
+}
